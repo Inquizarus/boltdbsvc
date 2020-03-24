54b421eeeaa222947adb11d130ea79b3d0033313
Conny Karlsson <connykarlsson9@gmail.com>
2020-03-24 21:12:02 +0100

    updated to new gorest version

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/boltdb/bolt v1.3.1
-	github.com/inquizarus/gorest v0.1.1
+	github.com/inquizarus/gorest v0.2.1
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/viper v1.6.2
 )
--- middlewares/request.go
+++ middlewares/request.go
@@ -9,10 +9,11 @@ import (
 
 // WithRequestLogging handles logging of all incoming requests
 func WithRequestLogging(logger logrus.StdLogger) gorest.Middleware {
-	return func(f http.HandlerFunc) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
+	return func(f http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
 			logger.Printf(`incoming %s request`, r.Method)
-			f(w, r)
+			f.ServeHTTP(w, r)
 		}
+		return http.HandlerFunc(fn)
 	}
 }
