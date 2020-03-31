52a055e7befb8892ee6e8d920b66518dc500f13a
Conny Karlsson <connykarlsson9@gmail.com>
2020-03-31 20:53:38 +0200

    testing of buckets handler

---
+++ handlers/buckets_test.go
@@ -0,0 +1,30 @@
+package handlers_test
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/inquizarus/boltdbsvc/handlers"
+	"github.com/inquizarus/boltdbsvc/storages"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBucketsHandlerWorking(t *testing.T) {
+	logger := logrus.New()
+	logger.Out = &SliceWriter{}
+	s := storages.MapStorage{
+		Map: map[string]map[string][]byte{
+			"bucket1": map[string][]byte{},
+		},
+	}
+	h := handlers.MakeListBucketHandler(&s, logger)
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/buckets", strings.NewReader(""))
+	h.Handle(w, r)
+	bbytes, _ := ioutil.ReadAll(w.Body)
+	assert.Equal(t, []byte(`{"meta":{"success":true},"data":["bucket1"]}`), bbytes)
+}
